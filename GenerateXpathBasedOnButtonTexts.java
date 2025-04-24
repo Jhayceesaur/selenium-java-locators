b83192c86f19699fb8e6f6c43b8cb55bd5f567be
John Carlo B. Alarva <jc.alarva1994@gmail.com>
2025-04-25 00:23:04 +0800

    Add files via upload

---
+++ GenerateXpathBasedOnButtonTexts.java
@@ -0,0 +1,34 @@
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class GenerateXpathBasedOnButtonTexts 
+{
+	public static void main(String[] args) throws InterruptedException 
+	{
+		String username = "Chickenjockey2015";
+		String password = "rahulshettyacademy";
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");
+
+		driver.findElement(By.cssSelector("#inputUsername")).sendKeys(username);
+		driver.findElement(By.cssSelector("input[type*='pass']")).sendKeys(password);
+		driver.findElement(By.xpath("//button[contains(@class,'submit')]")).click();
+		Thread.sleep(2000);
+		System.out.println(driver.findElement(By.tagName("p")).getText());
+		Assert.assertEquals(driver.findElement(By.tagName("p")).getText(), "You are successfully logged in.");
+		System.out.println(driver.findElement(By.xpath("//div[@class='login-container']/h2")).getText());
+		Assert.assertEquals(driver.findElement(By.xpath("//div[@class='login-container']/h2")).getText(), "Hello " + username + ",");
+		
+		//Xpath Locator based on button texts
+		driver.findElement(By.xpath("//button[text()='Log Out']")).click();
+		//driver.findElement(By.xpath("//*[text()='Log Out']")).click(); //Another way
+		
+		driver.close();
+	}
+}
